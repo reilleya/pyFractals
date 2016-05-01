356611dc9763f7f7734758c58b7a2aabde1af0fd
Andrew Reilley <areill1337@gmail.com>
2016-05-01 03:16:58 -0400

    Viewport is set correctly, did some shader tests

--- fractalwidget.py
+++ fractalwidget.py
@@ -5,13 +5,20 @@ from OpenGL import GL
 class FractalWidget(QGLWidget):
 	def __init__(self, parent):
 		QGLWidget.__init__(self, parent)
-		#self.setSizePolicy(QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum))
+		
+	def uploadShader(self, shaderText):
+		self.vertexShader = GL.glCreateShader(GL.GL_VERTEX_SHADER)
+		print self.vertexShader
+		GL.glShaderSource(self.vertexShader, "")
+		GL.glCompileShader(self.vertexShader)
+		print GL.glGetShaderInfoLog(self.vertexShader)
 
 	def initializeGL(self):
-		GL.glClearColor(0, 0, 0, 1)
+		GL.glClearColor(0, 1, 0, 1)
+		self.uploadShader(True)
 
 	def resizeGL(self, w, h):
-		pass
+		GL.glViewport(0, 0, w, h)
 		
 	def paintGL(self):
 		GL.glClear(GL.GL_COLOR_BUFFER_BIT)
